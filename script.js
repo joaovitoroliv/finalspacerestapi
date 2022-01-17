a2f47eaa5d9b6b17015ceb2f23f47979b11a63e6
João Vitor Oliveira Souza <62126757+joaovitoroliv@users.noreply.github.com>
2022-01-16 22:50:06 -0300

    Mudanca na logo, searchbar funcionando e cards iniciados ao abrir a pagina

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(app)
 // Criando elemento filho 'logo' 
 const logo = document.createElement('img');
 logo.setAttribute('id', 'logo');
-logo.src = 'https://i2.wp.com/lugarnenhum.net/wp-content/uploads/2018/08/final-space-logo-e1534353796350.jpg?fit=1280%2C640&ssl=1'
+logo.src = 'https://images.squarespace-cdn.com/content/v1/5616ac17e4b018d366f57f53/1575803544139-UGWQADWFPHLY85UGB16S/FINAL+SPACE+BANNER?format=1500w'
 
 // Criando um elemento filho 'div' e adicionando um atributo de nome 'class' com valor 'container' - <div class="container"></div>
 // Syntax: Element.setAttribute(name, value); 
@@ -16,11 +16,12 @@ container.setAttribute('class', 'container');
 
 // Barra de Busca
 const searchbar = document.createElement('div');
+searchbar.setAttribute('id', 'divSearchBar');
 const input = document.createElement('input');
 input.setAttribute('type', 'text');
 input.setAttribute('name', 'searchBar');
 input.setAttribute('id', 'searchBar');
-input.setAttribute('placeholder', 'Search for a character');
+input.setAttribute('placeholder', 'Procure por um personagem da série');
 searchbar.appendChild(input);
 
 
@@ -35,8 +36,38 @@ let response = fetch('https://finalspaceapi.com/api/v0/character/')
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    let filteredCharacters = data;
+
+    // Estado inicial com todos os personagens ao abrir a página
+    data.forEach(personagem => {
+      // Criar uma div com uma classe 'card' para cada filme
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      // Cria um elemento 'h1' e coloca como texto o titulo de cada filme
+      const nome = document.createElement('h1');
+      nome.textContent = personagem.name;
+
+      // Cria um elemento 'p' e coloca como texto o status de cada personagem
+      const status = document.createElement('p');
+      status.textContent = `Status: ${personagem.status}`
+
+      const origin = document.createElement('p');
+      origin.textContent = `Origin: ${personagem.origin}`;
+
+      // Cria um elemento 'img' e coloca a imagem da cada personagem
+      const img = document.createElement('img');
+      img.src = personagem.img_url;
+
+      container.appendChild(card);
+      card.appendChild(nome);
+      card.appendChild(status);
+      card.appendChild(origin);
+      card.appendChild(img);
+      console.log(card);
+    });
+
     // Filtro dos personagens
+    let filteredCharacters = data;
     input.addEventListener('keyup', tecla => {
       // Filtro dos personagens
 
@@ -47,9 +78,11 @@ let response = fetch('https://finalspaceapi.com/api/v0/character/')
         return character.name.toLowerCase().includes(searchString);
       });
       console.log(filteredCharacters);
-    });
- 
-    // Funcao para criar um card para cada personagem - for 
+
+      // "Limpar Tudo para poder Adicionar depois"
+      container.innerHTML = '';
+   
+      // Funcao para criar um card para cada personagem - for 
       filteredCharacters.forEach(personagem => {
         // Criar uma div com uma classe 'card' para cada filme
         const card = document.createElement('div');
@@ -78,3 +111,5 @@ let response = fetch('https://finalspaceapi.com/api/v0/character/')
         console.log(card);
       })
   });
+});
+ 
